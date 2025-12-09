bc71153f4de97e8c28feff15456d4b7b866119b0
Matheus <matheushsbaeta@gmail.com>
2025-12-09 19:28:41 -0300

    -feat: LIfe system

--- Assets/Game/Shared/Player/Scripts/PlayerController.cs
+++ Assets/Game/Shared/Player/Scripts/PlayerController.cs
@@ -6,10 +6,33 @@ public class PlayerController : MonoBehaviour
     public float speed;
     public BulletSystem bulletSystem;
     public float collisionDistance;
+    public SpriteRenderer spriteRenderer;
+    [SerializeField]
+    public PlayerHealth playerHealth;
+
+    private void Awake()
+    {
+        if (playerHealth == null) playerHealth = GetComponent<PlayerHealth>();
+    }
 
     public void OnMove(InputValue inputValue)
     {
         Vector2 movement = inputValue.Get<Vector2>();
         transform.position += (Vector3)movement * speed;
+    }
+
+    private void Update()
+    {
+        spriteRenderer.color = bulletSystem.playerHit ? Color.red : Color.white;
+        
+        if(bulletSystem.playerHit && !playerHealth.IsDead())
+        {
+            Debug.Log("HIT!!!");
+
+            playerHealth.TakeHit();
+
+            // Move player to start position
+            transform.position = new Vector3(0, -4, 0);
+        }
     }
 }
---
+++ Assets/Game/Shared/Player/Scripts/PlayerHealth.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerHealth : MonoBehaviour
+{
+    [SerializeField] private int maxHits = 3;
+    private int currentHits;
+
+    private void Awake()
+    {
+        this.currentHits = maxHits;
+    }
+
+    public void TakeHit()
+    {
+        this.currentHits--;
+        if (currentHits <= 0) Die();
+    }
+
+    public void Die()
+    {
+        SceneManager.LoadScene("GameOver");
+    }
+
+    public void IncreaseLife()
+    {
+        this. currentHits++;
+        if (currentHits > maxHits) currentHits = maxHits;
+    }
+
+    public bool IsDead()
+    {
+        return currentHits <= 0; 
+    }
+}
